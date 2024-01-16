5a7aba8275c8620e3165cd57a8e61ee489a4b021
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-16 08:24:12 -0800

    test: Add test for log_ride to Biker

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -45,5 +45,23 @@ RSpec.describe Biker do
             expect(biker.acceptable_terrain).to eq([:gravel, :hills])
         end
     end
+
+    describe '#log_ride(ride, time)' do
+        it 'will log rides completed and the times they were completed in' do
+            biker = Biker.new("Kenny", 30)
+            ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+            ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+
+            biker.learn_terrain!(:gravel)
+            biker.learn_terrain!(:hills)
+            biker.log_ride(ride1, 92.5)
+            biker.log_ride(ride1, 91.1)
+            biker.log_ride(ride2, 60.9)
+            biker.log_ride(ride2, 61.6)
+
+            expect(biker.rides).to eq({ride1 => [92.5, 91.1], ride2 => [60.9, 61.6]})
+        end
+    end
+
 end
 
