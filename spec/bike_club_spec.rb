5f9e0a8a34298e125fd9eda3de0509a48fd7d5a3
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-16 08:55:22 -0800

    test: Add tests for most_rides and best_time to BikeClub

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -30,10 +30,56 @@ RSpec.describe BikeClub do
             biker = Biker.new("Kenny", 30)
             biker2 = Biker.new("Athena", 15)
 
-            bike_club.add_biker(biker)
+            expect(bike_club.add_biker(biker)).to eq([biker])
+
             bike_club.add_biker(biker2)
 
             expect(bike_club.bikers).to eq([biker, biker2])
+        end
+    end
+
+    describe '#most_rides' do
+        it 'can tell you which biker in the club has logged the most rides' do
+            biker = Biker.new("Kenny", 30)
+            biker2 = Biker.new("Athena", 15)
+            ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+            ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+
+            biker.learn_terrain!(:gravel)
+            biker.learn_terrain!(:hills)
+            biker2.learn_terrain!(:gravel)
+            biker2.learn_terrain!(:hills)
+            
+            biker2.log_ride(ride1, 91.1)
+            
+            biker.log_ride(ride1, 92.5)
+            biker.log_ride(ride2, 60.9)
+            biker.log_ride(ride2, 61.6)
+
+            expect(bike_club.most_rides).to eq(biker)
+        end
+    end
+
+    describe '#best_time(ride)' do
+        it 'can tell you which biker has the best time on the given ride' do
+            biker = Biker.new("Kenny", 30)
+            biker2 = Biker.new("Athena", 15)
+            ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+            ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+
+            biker.learn_terrain!(:gravel)
+            biker.learn_terrain!(:hills)
+            biker2.learn_terrain!(:gravel)
+            biker2.learn_terrain!(:hills)
+            
+            biker2.log_ride(ride1, 91.1)
+            
+            biker.log_ride(ride1, 92.5)
+            biker.log_ride(ride2, 60.9)
+            biker.log_ride(ride2, 61.6)
+            
+            expect(bike_club.best_time(ride1)).to eq(biker2)
+            expect(bike_club.best_time(ride2)).to eq(biker)
         end
     end
 end
