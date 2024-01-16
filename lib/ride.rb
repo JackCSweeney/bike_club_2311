4ba970d7f5df0d74a87f582e565e23d49540b374
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-16 08:11:45 -0800

    feat: Add loop? and total_distance to Ride

--- lib/ride.rb
+++ lib/ride.rb
@@ -2,7 +2,6 @@ class Ride
 
     attr_reader :name,
                 :distance,
-                :loop,
                 :terrain
 
     def initialize(info)
@@ -10,6 +9,14 @@ class Ride
         @distance = info[:distance]
         @loop = info[:loop]
         @terrain = info[:terrain]
+    end
+
+    def loop?
+        @loop
+    end
+
+    def total_distance
+        @loop ? @distance : (@distance * 2).round(1)
     end
 
 end
